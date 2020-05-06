2769a6a152bb239e7b27d971f483979614e1c980
Sofia131 <sofia.trombini98@gmail.com>
2020-05-06 14:17:34 +0200

    primo commit branch sofi

---
+++ src/dynsim_2link_planar/slros_initialize.h
@@ -0,0 +1,36 @@
+#ifndef _SLROS_INITIALIZE_H_
+#define _SLROS_INITIALIZE_H_
+
+#include "slros_busmsg_conversion.h"
+#include "slros_generic.h"
+
+extern ros::NodeHandle * SLROSNodePtr;
+extern const std::string SLROSNodeName;
+
+// For Block dynsim_2link_planar/Subscribe
+extern SimulinkSubscriber<std_msgs::Float64MultiArray, SL_Bus_dynsim_2link_planar_std_msgs_Float64MultiArray> Sub_dynsim_2link_planar_16;
+
+// For Block dynsim_2link_planar/Publish
+extern SimulinkPublisher<sensor_msgs::JointState, SL_Bus_dynsim_2link_planar_sensor_msgs_JointState> Pub_dynsim_2link_planar_22;
+
+// For Block dynsim_2link_planar/Publish1
+extern SimulinkPublisher<rosgraph_msgs::Clock, SL_Bus_dynsim_2link_planar_rosgraph_msgs_Clock> Pub_dynsim_2link_planar_50;
+
+// For Block dynsim_2link_planar/Publish2
+extern SimulinkPublisher<sensor_msgs::JointState, SL_Bus_dynsim_2link_planar_sensor_msgs_JointState> Pub_dynsim_2link_planar_124;
+
+// For Block dynsim_2link_planar/2link_planar robot dynamic model/Subsystem/Get Parameter
+extern SimulinkParameterGetter<real64_T, double> ParamGet_dynsim_2link_planar_112;
+
+// For Block dynsim_2link_planar/2link_planar robot dynamic model/Subsystem/Get Parameter1
+extern SimulinkParameterGetter<real64_T, double> ParamGet_dynsim_2link_planar_113;
+
+// For Block dynsim_2link_planar/2link_planar robot dynamic model/Subsystem/Get Parameter2
+extern SimulinkParameterGetter<real64_T, double> ParamGet_dynsim_2link_planar_117;
+
+// For Block dynsim_2link_planar/2link_planar robot dynamic model/Subsystem/Get Parameter3
+extern SimulinkParameterGetter<real64_T, double> ParamGet_dynsim_2link_planar_118;
+
+void slros_node_init(int argc, char** argv);
+
+#endif
