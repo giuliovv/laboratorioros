3da47a4eddb523bb3624808703417e2515524f30
giuliovv <giulio09@marcov.it>
2020-05-13 23:06:25 +0200

    Corrette inerzie urdf

--- laboratorioros-master_2/laboratorioros-master/matlab/dynsim_2link_planar_ert_rtw/dynsim_2link_planar.cpp
+++ laboratorioros-master_2/laboratorioros-master/matlab/dynsim_2link_planar_ert_rtw/dynsim_2link_planar.cpp
@@ -9,7 +9,7 @@
 //
 // Model version                  : 1.128
 // Simulink Coder version         : 9.3 (R2020a) 18-Nov-2019
-// C/C++ source code generated on : Wed May 13 13:33:05 2020
+// C/C++ source code generated on : Wed May 13 22:32:24 2020
 //
 // Target selection: ert.tlc
 // Embedded hardware selection: Generic->Unspecified (assume 32-bit Generic)
